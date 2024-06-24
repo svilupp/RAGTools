1b869fd5773b9d6caa6435690b693e971a876968
Documenter.jl <documenter@juliadocs.github.io>
2024-06-24 20:13:27 +0000

    build based on 69c5892

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RAGTools","category":"page"},{"location":"#RAGTools","page":"Home","title":"RAGTools","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RAGTools.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RAGTools]","category":"page"}]
+}
